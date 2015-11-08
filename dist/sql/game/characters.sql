3f77e2cc97b52d2a94df9a3d55256afef8fd14e4
gamer <gamer2015@foxmail.com>
2015-11-08 23:03:24 +0800

    大陆商城SQL脚本

--- dist/sql/game/characters.sql
+++ dist/sql/game/characters.sql
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `characters` (
   `vitality_points` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
   `createDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `language` VARCHAR(2) DEFAULT NULL,
-  `GamePoint` bigint(13) unsigned NOT NULL DEFAULT '0',
+  `game_points` bigint(20) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`charId`),
   KEY `account_name` (`account_name`),
   KEY `char_name` (`char_name`),
